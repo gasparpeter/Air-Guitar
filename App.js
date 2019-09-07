b5c2b1617babc8e332405a105aaa864f71ea451f
Peter Gaspar <petihappygaspar@gmail.com>
2019-09-07 22:05:49 +0300

    - recognizes the hand

--- App.js
+++ App.js
@@ -16,11 +16,24 @@ const video = document.querySelector('#video');
 const audio = document.querySelector('#audio');
 let model;
 
-handTrack.startVideo(video)
-    .then(status => {
+handTrack.startVideo(video).then(status => {
         if (status) {
             navigator.getUserMedia({video: {}}, stream => {
                 video.srcObject = stream;
-            })
+
+                setInterval(runDetection, 100)
+            },
+                err => console.log(err)
+            );
         }
     });
+
+function runDetection() {
+    model.detect(video).then(predictions => {
+        console.log(predictions)
+    });
+}
+
+handTrack.load(modelParams).then(lmodel => {
+    model = lmodel
+});
